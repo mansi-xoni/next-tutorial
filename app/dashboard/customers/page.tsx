be518ce9296801ba561c678eed0a91fd70c621b0
Mansi Soni <mansixoni@gmail.com>
2025-03-08 20:45:42 +0530

    initial commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Customers = (props: Props) => {
+  return (
+    <div>mera Customers</div>
+  )
+}
+
+export default Customers
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+type Props = {}
+
+const Dashbaord = (props: Props) => {
+  return (
+    <div>mera Dashbaord</div>
+  )
+}
+
+export default Dashbaord
